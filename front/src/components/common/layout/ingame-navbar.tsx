27050d73565d3f8cfa0ea5e78b17aaf39732fbaf
DvK42 <guillaume.couvidou@live.fr>
2026-06-30 00:15:30 +0200

    🐛 Time up timer

--- front/src/components/common/layout/ingame-navbar.tsx
+++ front/src/components/common/layout/ingame-navbar.tsx
@@ -15,7 +15,7 @@ import GameStepIndicator from '../game/game-step-indicator';
 import { GameGlobalStepEnum } from '@/utils/enums';
 
 const IngameNavbar = () => {
-  const { game } = useGame();
+  const { game, timeUp } = useGame();
   const { isTokenInitialized } = useApiClient();
   const tg = useTranslations();
 
@@ -24,7 +24,7 @@ const IngameNavbar = () => {
       case GameGlobalStepEnum.NEW:
         return <GameJoinCode joinCode={game?.joinCode ?? ''} />;
       case GameGlobalStepEnum.RUNNING:
-        return game?.stepEndAt && isTokenInitialized ? <Timer stepEndAt={game.stepEndAt} /> : null;
+        return game?.stepEndAt && isTokenInitialized ? <Timer stepEndAt={game.stepEndAt} onTimeOut={timeUp} /> : null;
       case GameGlobalStepEnum.FINISH:
         return null;
       default:
