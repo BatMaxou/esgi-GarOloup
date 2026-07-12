414d35b22174c1ce2d0944e708d6ce517436ba6b
DvK42 <guillaume.couvidou@live.fr>
2026-07-12 15:19:13 +0200

    🐛 Bad condition forgotten

--- front/src/components/ui/molecules/icon/player-lover-partner-item.tsx
+++ front/src/components/ui/molecules/icon/player-lover-partner-item.tsx
@@ -2,11 +2,11 @@
 
 import { Player } from '@/utils/types';
 import Icon from '../../atoms/icon';
-import { isLoverRole, useLover } from '@/contexts/roles/lover-context';
+import { useLover } from '@/contexts/roles/lover-context';
 
 const PlayerLoverPartnerIcon = ({ currentPlayer, player }: { currentPlayer: Player; player: Player }) => {
   const { partnerPlayerId } = useLover();
-  if (partnerPlayerId === player.id || isLoverRole(currentPlayer.role)) {
+  if (partnerPlayerId === player.id) {
     return <Icon name="heart" className="w-5 h-5 text-pink-700" />;
   }
   if (partnerPlayerId === currentPlayer.id) {
